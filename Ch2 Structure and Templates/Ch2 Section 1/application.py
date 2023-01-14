1a23bdde012ebba4d4e4e80c14c9ff1314fb06ad
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-01-14 11:53:54 +0100

    Add files via upload

---
+++ Ch2 Structure and Templates/Ch2 Section 1/application.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/home')
+def home():
+  return render_template('home.html', title='home')
+
+@application.route('/methodology')
+def methodology():
+  return render_template('methodology.html', title='methodology')
+
+@application.route('/app_calculator')
+def app_calculator():
+    return render_template('app_calculator.html', title='app_calculator')
+
+if __name__=='__main__':
+  application.run(debug=True)  
