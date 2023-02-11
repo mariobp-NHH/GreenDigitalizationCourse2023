c5bea08c578e95358840cc4f3eacd671e4c3a926
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-02-11 22:53:29 +0100

    Add files via upload

---
+++ Ch8 Forms/Final2 Register HTML/capp/users/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, Blueprint, redirect, flash, url_for
+from capp.users.forms import RegistrationForm
+
+users=Blueprint('users',__name__)
+
+@users.route('/register', methods=['GET','POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+      return redirect(url_for('home.home_home'))
+  return render_template('users/register.html', title='register', form=form)
+
+@users.route('/login', methods=['GET','POST'])
+def login():
+  return render_template('users/login.html', title='login')
+
+
