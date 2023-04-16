12838574c04482498424cabb8d4a555c1e1e5fd4
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2023-04-16 10:02:31 +0200

    Update __init__.py

--- Ch14 Heroku/capp/__init__.py
+++ Ch14 Heroku/capp/__init__.py
@@ -7,11 +7,12 @@ import os
 application = Flask(__name__)
 
 ### Code GitHub
-# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-# DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-# DBVAR = 'postgresql://postgres:7714LLUVIa!@awseb-e-jvjmjwsd8n-stack-awsebrdsdatabase-iqxwwenomoah.cfn432aqykfx.eu-north-1.rds.amazonaws.com:5432/ebdb'
-# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-# application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# DBVAR=os.environ['DATABASE_URL']
+# DBVAR="postgresql://osgiedhtruutkx:os.environ.get(‘DB_PASSWORD’)@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d8scuqpg5glq3f"
+DBVAR="postgresql://osgiedhtruutkx:835d46742b5eaba7a35135c701e4137b122ea454437d97d958238ffbe416457a@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d8scuqpg5glq3f"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
 ### Code computer
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
