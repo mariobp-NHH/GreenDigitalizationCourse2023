1568ad59a540288a32b43c643e1706a5d8469bc4
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2025-08-26 12:20:15 +0200

    Update application.py

--- Ch1 Installation and First App/Ch1 Installation and First App Initial/application.py
+++ Ch1 Installation and First App/Ch1 Installation and First App Initial/application.py
@@ -4,22 +4,8 @@ application = Flask(__name__)
 @application.route('/')
 @application.route('/home')
 def home():
-    return "<h1>Welcome home!</h1>" \
-    "<p>This is our methodology:</p> <button>Link</button>" \
-    "<ul>" \
-    " <li>Developer 1...</li>" \
-    " <li>Developer 2...</li>" \
-    " <li>Developer 3...</li>" \
-    "</ul>"
+  return "<h1>Welcome Home</h1>"
 
-
-@application.route('/methodology')
-def methodology():
-    return "<h1>Methodology</h1>"
-
-@application.route('/carbon_app')
-def carbon_app():
-    return "<h1>Carbon App</h1>"
-
-if __name__ =='__main__':
-    application.run(debug=True)
+if __name__=='__main__':
+    application.run(debug=True)  
+  
