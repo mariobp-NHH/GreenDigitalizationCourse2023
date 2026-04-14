e1baa91044433fa75663886537da934154f97bdc
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2026-04-14 21:48:43 +0200

    Delete Ch13 Chart/capp_2026 directory

--- Ch13 Chart/capp_2026/home/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, Blueprint
-from capp.models import User, Transport
-
-home=Blueprint('home',__name__)
-
-@home.route('/')
-@home.route('/home')
-def home_home():
-  return render_template('home.html')
